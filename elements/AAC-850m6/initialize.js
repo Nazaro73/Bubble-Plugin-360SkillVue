1a450dbeb13ae45e2164bc17957240cf504677cd
Alexandre Sanchez <alex73630@gmail.com>
2025-01-15 05:01:00 +0100

    Automatic commit from Bubble

--- elements/AAC-850m6/initialize.js
+++ elements/AAC-850m6/initialize.js
@@ -1,5 +1,11 @@
 function(instance, context) {
+	const video = document.createElement('video');
+	
+	// video.src = 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
+	video.controls = true;
+	video.style.objectFit = 'contain'
+	video.style.width = '100%';
+	video.style.height = '100%';
 
-
-
+	$(instance.canvas).append(video);
 }
--- elements/AAC-850m6/update.js
+++ elements/AAC-850m6/update.js
@@ -1,5 +1,10 @@
 function(instance, properties, context) {
 
+	const video = $(instance.canvas).find('video')[0];
+	if (video) {
 
-
+		if (video.src !== properties.video_url) {
+			video.src = properties.video_url;
+		}
+	}
 }
