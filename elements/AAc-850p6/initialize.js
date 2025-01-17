d0e97be02ce81eae6fe924ecbab3d6f657adbff1
Alexandre Sanchez <alex73630@gmail.com>
2025-01-17 17:21:12 +0100

    Automatic commit from Bubble

--- elements/AAc-850p6/initialize.js
+++ elements/AAc-850p6/initialize.js
@@ -1,5 +1,8 @@
 function(instance, context) {
-
-
-
+	
+	const elemId = crypto.randomUUID()
+    
+    $(instance.canvas).append(`<div id="${elemId}"></div>`)
+    
+    window.plugin360SkillVueVideoRecord.init()
 }
