ab24d87d4f7c0c32aa261ac363d2176084e527e8
Alexandre Sanchez <alex73630@gmail.com>
2025-01-17 06:46:05 +0100

    Automatic commit from Bubble

--- elements/AAS-850n6/update.js
+++ elements/AAS-850n6/update.js
@@ -1,21 +1,5 @@
 function(instance, properties, context) {
 
-    const currLang = properties.language || 'fr'
-
-  //Load any data 
-    const languageNames = new Intl.DisplayNames([currLang], {
-      type: 'language',
-      languageDisplay: 'standard'
-    });
-    const languageCodes = ISO6391.getAllCodes()
-    const languageList = languageCodes.map(code => {
-        let lang = languageNames.of(code)
-        if (lang === code) { return null }
-        let item = { text: String(lang).charAt(0).toUpperCase() + String(lang).slice(1), value: code }
-        return item
-    }).filter(Boolean)
-
-
   //Do the operation
     const currLang = properties.language || 'fr'
 
--- elements/AAW-850ne/update.js
+++ elements/AAW-850ne/update.js
@@ -1,6 +1,19 @@
 function(instance, properties, context) {
     // Translations: { Langue: String, Texte: String }[]
-	const translations = properties.translations
+	let translations = []
+    if (properties.translations && properties.translations.length() > 0) {
+        translations = properties.translations
+            .get(0, properties.translations.length()) // Get all items in the list
+            .map(t => ({
+                Langue: t.get("langue_text"), // Access the "Langue" field
+                Texte: t.get('texte_text')   // Access the "Texte" field
+            }));
+
+        console.log(translations); // Debugging output
+        instance.data.translations = translations; // Store for later use
+    }
+    
+    console.log(translations, properties.translations)
 
 	const lang = properties.language
     const fallbackLang = properties.fallbackLang || 'fr'
@@ -10,6 +23,8 @@ function(instance, properties, context) {
     const fallbackFirstTranslation = translations[0]
 
     const text = requestedTranslation?.Texte || fallbackTranslation?.Texte || fallbackFirstTranslation?.Texte || ''
+    
+    console.log(text)
     
 	instance.publishState('text', text)
 }
